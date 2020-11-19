c10deabe38ee2c9eb23138d72e4bba3ddbcf00cd
Aliya Almas <itisaliya@gmail.com>
2020-11-20 01:33:58 +0600

    admin additions

--- ourplatform/admin.py
+++ ourplatform/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
-
+from ourplatform.models import GithubEvent,TelegramEvent, SlackEvent, Project, ProjectAndUser
 # Register your models here.
+admin.site.register(GithubEvent)
+admin.site.register(TelegramEvent)
+admin.site.register(SlackEvent)
+admin.site.register(Project)
+admin.site.register(ProjectAndUser)
